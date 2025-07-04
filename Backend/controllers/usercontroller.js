1ba064d3f779fa0147fc620823771bfd6f561ecb
shanubirla <shanubirlashanubirla@gmail.com>
2025-07-04 20:44:23 +0530

    allow different site

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -104,7 +104,7 @@ export const login = async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'None' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'None' ,secure: true}).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
